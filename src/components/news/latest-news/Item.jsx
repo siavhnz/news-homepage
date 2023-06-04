d39012363864b5323b0cf616ed3bc5128af4c9c7
siavhnz <siavhnz@gmail.com>
2023-06-04 12:23:57 +0330

    change font weight

--- src/components/news/latest-news/Item.jsx
+++ src/components/news/latest-news/Item.jsx
@@ -1,7 +1,7 @@
 const LatestNewsItem = ({ title, description }) => {
   return (
     <div className="mb-5 border-b border-dark-grayish-blue pb-7 last:mb-0 last:border-none">
-      <h3 className="pt-1 text-[1.27rem] font-extrabold text-white transition-all duration-700 hover:cursor-pointer hover:text-soft-orange">
+      <h3 className="pt-1 text-[1.3rem] font-bold text-white transition-all duration-700 hover:cursor-pointer hover:text-soft-orange">
         <a href="#">{title}</a>
       </h3>
       <p className="pt-1 leading-7 text-dark-grayish-blue">{description}</p>
